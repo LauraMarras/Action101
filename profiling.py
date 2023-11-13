492b4e8754233bf44579f7a84109cbee3c251042
Laura Marras <l.marras@student.maastrichtuniversity.nl>
2023-11-13 16:05:15 +0100

    Solve parallelization errors, miscellaneous

--- profiling.py
+++ profiling.py
@@ -1,72 +1,66 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['BLIS_NUM_THREADS'] = '1'
+
+from check_collinearity import canoncorrelation
+from canon_corr_fmri import permutation_schema, run_canoncorr, gen_fmri_signal, gen_correlated_data
+import numpy as np
+import time
+import multiprocessing as mp
+
 if __name__ == '__main__':
 
-    import os
-    os.environ['OPENBLAS_NUM_THREADS'] = '1'
-    os.environ['MKL_NUM_THREADS'] = '1'
-    os.environ['OMP_NUM_THREADS'] = '1'
-    os.environ['BLIS_NUM_THREADS'] = '1'
-
-    #from check_collinearity import canoncorrelation
-    from canon_corr_fmri import generate_signal, permutation_schema, run_canoncorr
-    import numpy as np
-    import time
-    import multiprocessing as mp
-    
     t = time.time()
 
     # Set parameters
-    n_rois = 10#400
+    n_rois = 10 #400
     n_voxels = 100
-    n_perms = 10 #1000
+    n_perms = 500 #1000
     n_tpoints = 1614
-    chunk_size = 15
+    chunk_size = 15 # add check max action duration
     seed = 0
 
-    # Generate simulated data (to be replaced with loading of real data)
-    data = {r: generate_signal() for r in range(n_rois)}
-
+    
     # Set data_path as the path where the csv files containing single domain matrices are saved, including first part of filename, up to the domain specification (here I specify 'tagging_carica101_group_2su3_convolved_' for example)
-    data_path = 'C:/Users/laura/OneDrive/Documenti/PhD/ProgettoLorenzo/Data_Code/Data/Carica101_Models/Domains/tagging_carica101_group_2su3_convolved_'
+    data_path = 'Data/Carica101_Models/Domains/tagging_carica101_group_2su3_convolved_'
 
     # Set out_path as the path where to save results figures
-    out_path = 'C:/Users/laura/OneDrive/Documenti/PhD/ProgettoLorenzo/Data_Code/Results/Collinearity/New/'
+    out_path = 'Results/Collinearity/New/'
 
     # Load Models
     domains_list = ['space_movement', 'agent_objective', 'social_connectivity', 'emotion_expression', 'linguistic_predictiveness']
     domains = {d: np.loadtxt(data_path + '{}.csv'.format(d), delimiter=',', skiprows=1)[:, 1:] for d in domains_list}
 
+    # Generate fake correlated data starting from domain
+    data = {r: gen_correlated_data(domains['space_movement'], n_voxels, noise=0.1) for r in range(n_rois)}
+
+    # Generate fake simulated data (simple oscillations)
+    #data = {r: gen_fmri_signal() for r in range(n_rois)}
+    
     # Generate permutation schema
-    perm_schema = permutation_schema(n_tpoints, n_perms=n_perms)
+    perm_schema = permutation_schema(n_tpoints, n_perms=n_perms, chunk_size=chunk_size)
 
     # Initialize results matrix
-    results = np.full((n_rois, n_perms+1, len(domains)), np.nan)
+    result_matrix = np.full((n_rois, n_perms+1, len(domains)), np.nan)
 
     #### Get R2 With pool
     t1 = time.time()
+    results_pool = []
 
-    with mp.Pool(mp.cpu_count()) as pool:
-        t2 = time.time() - t1
-        
-        results = np.array([pool.apply(run_canoncorr, args=(roi, data, perm_schema, domains)) for roi in range(n_rois)])
-        
-        t3  = time.time() - t1
-        
-        for roi in range(n_rois):
-            results[roi, :, :] = run_canoncorr(roi, data, perm_schema, domains)
-        
-        t4 = time.time() - t1
+    pool = mp.Pool(4)
+
+    for r, roi in data.items():
+        result_pool = pool.apply_async(run_canoncorr, args=(roi, perm_schema, domains, True))
+        results_pool.append(result_pool)
     
-    #### Without pool
-    t5 = time.time() - t1
-
-    for roi in range(n_rois):
-            results[roi, :, :] = run_canoncorr(roi, data, perm_schema, domains)
+    pool.close()
     
-    t6 = time.time() - t1
 
-    print('time before pool  ', t1 - t)
-    print('time to call pool  ', t2)
-    print('time to run with pool.apply  ', t3 - t2)
-    print('time to run with pool for loop  ', t4 - t3)
-    print('time to finish pool  ', t5 - t4)
-    print('time to run without for loop  ', t6 - t5)
+    for result_pool in results_pool:
+        njob = result_pool._job
+        result_matrix[njob, :, :] = result_pool.get()
+        
+    
+    print('d')
