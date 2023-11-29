0ccef615df289708d0c2df4b189d6551b9e0ca1b
LauraMarras <laura.marras@imtlucca.it>
2023-11-29 16:25:10 +0000

    adjusted rot trans function

--- motion_regressors.py
+++ motion_regressors.py
@@ -37,41 +37,39 @@ def rotate_mri(mriVolume, upscale, movement_offsets):
     #mriVolume = mriVolume.astype("uint16")
    
     # Upsample volume
-    #mriVolume_res = zoom(mriVolume, upscale, mode='nearest')
+    mriVolume_res = zoom(mriVolume, upscale, mode='nearest')
 
-    # Rotation
-    ## Convert rotation values to radians
-    rotation_radians = np.deg2rad(movement_offsets[:3])
+    #rotation matrix
+    rotation = transform.SimilarityTransform(rotation=np.radians(movement_offsets[:3]),dimensionality=3)
+    shift = transform.SimilarityTransform(translation=-np.array(mriVolume_res.shape[:3])/2,dimensionality=3)
+    # translation matrix
+    translation = transform.SimilarityTransform(translation=movement_offsets[3:6],dimensionality=3) 
+
+    # Compose transforms by multiplying their matrices
+    matrix = translation.params @ np.linalg.inv(shift.params) @ rotation.params @ shift.params
+
+    # apply transforms to coordinates
+    coords = np.rollaxis(np.indices(mriVolume_res.shape), 0, len(mriVolume_res.shape)+1)
+    coords = np.append(coords,np.ones((coords.shape[0],coords.shape[1],coords.shape[2],1)), axis=3)
+    trans_coords = np.matmul(coords, np.linalg.inv(matrix).T) # .T)
+    trans_coords = np.delete(trans_coords,3,axis=3).astype(int)
+
+   # pad = np.max((np.abs(trans_coords.max() - trans_coords.shape[0]), np.abs(trans_coords.min())))
+    pad = np.max(np.concatenate((np.abs(np.max(trans_coords, (0,1,2)) - (np.array(trans_coords.shape[:3])-1)), np.abs(np.min(trans_coords, (0,1,2))))))
+    mripadded = np.pad(mriVolume_res, pad, mode='constant')
+    trans_coords = trans_coords+pad
+    final = np.full(mriVolume_res.shape, np.nan)
+    for x in range(trans_coords.shape[0]):
+        for y in range(trans_coords.shape[1]):
+            for z in range(trans_coords.shape[2]):
+                final[x,y,z] = mripadded[tuple(trans_coords[x,y,z,:])] 
+
+
+    # scale down to original resolution
+    mriVolume_rot_dis_res = zoom(final, 1/upscale, mode='nearest')
+   # mriVolume_rot_dis_res = mriVolume_rot_dis_res([1:size(mriVolume,1)],[1:size(mriVolume,2)],[1:size(mriVolume,3)]);
     
-    ## Create rotation matrices for each axis and combine them
-    rotation_matrices = []
-    for i in range(3):
-        rotation_axis = np.zeros(3)
-        rotation_axis[i] = 1  # Set the corresponding axis to 1 for rotation
-        rotation = Rotation.from_rotvec(rotation_radians[i] * rotation_axis)
-        rotation_matrices.append(rotation.as_matrix())
-
-    ## Combine rotation matrices for each axis
-    combined_rotation_matrix = np.eye(4)
-    for rot_matrix in rotation_matrices:
-        combined_rotation_matrix = np.dot(combined_rotation_matrix, np.vstack((rot_matrix, [0, 0, 0, 1])))
-
-
-    # Translation
-    translation_matrix = np.eye(4)
-    translation_matrix[:3, 3] = movement_offsets[3:]
-
-    # Combine translation and rotation matrices
-    combined_matrix = np.dot(translation_matrix, combined_rotation_matrix)
-
-    # Apply the combined transformation to the original 3D matrix
-    transformed_matrix = np.dot(mriVolume, combined_matrix)
-
-    # # scale down to original resolution
-    # #mriVolume_rot_dis_res=imresize3(mriVolume_rot_dis,size(mriVolume), 'nearest');
-    # #mriVolume_rot_dis_res=mriVolume_rot_dis_res([1:size(mriVolume,1)],[1:size(mriVolume,2)],[1:size(mriVolume,3)]);
-
-    return transformed_matrix
+    return mriVolume_rot_dis_res
 
 
 if __name__ == '__main__':
@@ -79,14 +77,14 @@ if __name__ == '__main__':
     nTRs = 260
     scaling = np.concatenate(((np.random.randn(3)/10), (np.random.randn(3)/3)), 0)
     window_size = 3
-    movement_offsets = generate_movement_regressors(nTRs, scaling, window_size)
-
+   # movement_offsets = generate_movement_regressors(nTRs, scaling, window_size)
+    movement_offsets = np.array([0,0,90,0,0,0]) 
     
     data = image.load_img('datasets/run1_template.nii')
     data_map = data.get_fdata()
     volume = data_map[:,:,:,0]
 
-    translated = rotate_mri(volume, 6, movement_offsets[0])
+    translated = rotate_mri(volume, 6, movement_offsets)
     
     print('d')
 
