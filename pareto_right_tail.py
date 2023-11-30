172aa54fc56879029ac6d0bbf8a8bd19b88c5700
LauraMarras <laura.marras@imtlucca.it>
2023-11-30 11:57:22 +0000

    create pareto function

---
+++ pareto_right_tail.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.stats import rv_continuous, pareto
+
+
+def pareto_right(null_distro, critical_value, critical_p, tail_percentile=0.9):
+
+    permutations = null_distro.shape[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return pvalue, critical_value_at_p
