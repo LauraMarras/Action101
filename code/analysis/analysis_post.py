af735028e2709b6de35688a0eb9cdf0529eed61c
LauraMarras <laura.marras@imtlucca.it>
2024-08-08 21:26:58 +0000

    add t-sne on correct R, plot, paired ttest, create nifti

--- code/analysis/analysis_post.py
+++ code/analysis/analysis_post.py
@@ -5,7 +5,7 @@ from canonical_correlation_funcs import extract_roi, run_cca_all_subjects
 from stats_funcs import get_pvals_sub, get_pvals_group, save_nifti
 from sklearn.manifold import MDS, TSNE
 from scipy.stats import false_discovery_control as fdr
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, wilcoxon, ttest_rel
 import itertools
 from utils.eval_kmeans import evalKMeans
 from matplotlib import pyplot as plt
@@ -88,6 +88,31 @@ def ttest_domains(results, dom_combs, dom_combs_str, fdr_opt=True, save=False):
 
     return ts, pvals
 
+def ttest_rel_domains(results, dom_combs, dom_combs_str, fdr_opt=True, save=False):
+    
+    # Initialize results matrices
+    ts = np.empty((results.shape[1], len(dom_combs)))
+    pvals = np.empty((results.shape[1], len(dom_combs)))
+
+    # Run ttest_rel for each ROI and each domains pairing
+    for roi in range(results.shape[1]):  
+        for comb in dom_combs:
+            d1 = results[:, roi, comb[0]]
+            d2 = results[:, roi, comb[1]]
+            t, pval = ttest_rel(d1, d2)
+            ts[roi,comb] = t 
+            pvals[roi,comb] = pval
+    
+    # FDR correction for multiple comparisons
+    if fdr_opt:
+        pvals = np.reshape(fdr(np.ravel(pvals)), pvals.shape)
+
+    # Save results
+    if save:
+        np.savez('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/ttest/ttest_rel_cca', tstat=ts, pvals=pvals, domains_contrasts=dom_combs_str, FDRcorrection=fdr_opt)
+
+    return ts, pvals
+
 def clustering(results_group, n_clust_max, atlas_file):
 
     silhouette_avg, clusters_labels = evalKMeans(range(2, n_clust_max+1), results_group, print_otp=False)
@@ -112,6 +137,37 @@ def clustering(results_group, n_clust_max, atlas_file):
 
     return silhouette_avg, clusters_labels
 
+def save_nifti(results, briks, atlas_file='Schaefer200', savepath='', ROIsmask=[]):
+    
+    atlas = image.load_img('/data1/Action_teresi/CCA/atlas/Schaefer_7N_{}.nii.gz'.format(atlas_file[-3:]))
+    atlas_rois = np.unique(atlas.get_fdata()).astype(int)
+    atlas_rois = np.delete(atlas_rois, np.argwhere(atlas_rois==0))
+
+    if len(ROIsmask) <= 0:
+        ROIsmask = atlas_rois
+
+    x,y,z = atlas.get_fdata().shape
+    
+    image_final = np.squeeze(np.zeros((x,y,z,briks)))
+
+    for r, roi in enumerate(ROIsmask):
+        x_inds, y_inds, z_inds = np.where(atlas.get_fdata()==roi)
+        
+        image_final[x_inds, y_inds, z_inds] = results[:, r]
+
+    img = image.new_img_like(atlas, image_final, affine=atlas.affine, copy_header=False)
+    img.to_filename(savepath)
+
+    return
+
+def create_ttest_nifti(ts, pvals, dom_combs, dom_combs_str):
+        for c, comb in enumerate(dom_combs):
+            cres = np.expand_dims(np.vstack((ts[:,c], pvals[:,c])).T, axis=0)
+            path = '/data1/Action_teresi/CCA/cca_results/group/ttest/ttest_{}.nii.gz'.format(dom_combs_str[c][0][:5] + 'vs' + dom_combs_str[c][1][:5])
+            save_nifti(cres, 2, atlas_file, path, ROIs_2keep+1)
+
+        return
+
 if __name__ == '__main__': 
     
     # Set parameters
@@ -120,23 +176,26 @@ if __name__ == '__main__':
     plot_clust = False
     suffix = '_pca_variancepart'
     plot_tsne = False
-    metric='cosine'
+    metric='euclidean'
 
     # Load group results and filter ROIs with significant R2 for full model
     pvals_group_fm = np.load('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/group/CCA_res_group_pcanoz_fullmodel_6doms_{}_maxT.npz'.format(atlas_file), allow_pickle=True)['pvals_group']
     ROIs_2keep = np.unique(np.where(np.squeeze(pvals_group_fm) < 0.05)[0])
     results_group = np.load('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/group/CCA_res_group{}_{}.npz'.format(suffix, atlas_file), allow_pickle=True)['results_group'][ROIs_2keep,:]
 
+    results_filtered_vp = np.load('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/group/CCA_res_allsubs_singledoms_filtered.npy', allow_pickle=True)
+    results_vp_avg = results_filtered_vp[-1,:,:]
+    results_vp = results_filtered_vp[:-1,:,:]
 
     # tSNE
-    tsne = TSNE(random_state=0, metric=metric, init='random', perplexity=5)
-    results_tsne = tsne.fit_transform(results_group)
+    tsne = TSNE(random_state=0, metric=metric, init='random', perplexity=5, n_iter=5000)
+    results_tsne = tsne.fit_transform(results_vp_avg)
 
     if plot_tsne:
         plt.figure()
         scatt = plt.scatter(results_tsne[:,0], results_tsne[:,1])
         plt.title('tSNE on {} distance btw  ROIs R2'.format(metric))
-
+        plt.suptitle('KL-divergence = {}'.format(tsne.kl_divergence_))
         ax = plt.gca()
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
@@ -144,33 +203,47 @@ if __name__ == '__main__':
         ax.spines['bottom'].set_visible(False)
         ax.axes.xaxis.set_visible(False)
         ax.axes.yaxis.set_visible(False)
-        plt.savefig('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/clustering/tsne_{}.png'.format(metric))
+        plt.savefig('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/clustering/tsne_{}_new.png'.format(metric))
 
     # Save
-    np.savetxt('/data1/Action_teresi/CCA/cca_results/group/CCA_res_group_filtered'.format(metric), results_group)
+    np.savetxt('/data1/Action_teresi/CCA/cca_results/group/CCA_res_group_filtered', results_group)
     
     # Clustering
-    n_clust_max=10
-    silhouette_avg, clusters_labels = clustering(results_group, n_clust_max, atlas_file)
-
-    mds = MDS().fit_transform(results_group)
-
+    n_clust_max=5
+    silhouette_avg, clusters_labels = clustering(squareform(pdist(results_tsne, metric=metric)), n_clust_max, atlas_file)
+    clust = np.argmax(silhouette_avg)
+    
     # Plot
     if plot_clust:
-        for clust in range(clusters_labels.shape[0]):
-            plt.figure()
-            scatt = plt.scatter(mds[:,0], mds[:,1], c=clusters_labels[clust])
-            plt.legend(*scatt.legend_elements(), loc='lower right', title='Clusters', ncol=2, frameon=False, borderpad=-1, labelspacing=0.1, borderaxespad=0.1, columnspacing=0.1, handletextpad=-0.5)
-            plt.title('Clustering ROIs')
-            plt.suptitle('Silhouette score for {} clusters = {}'.format(clust+2, silhouette_avg[clust]))
-            ax = plt.gca()
-            ax.spines['right'].set_visible(False)
-            ax.spines['top'].set_visible(False)
-            ax.spines['left'].set_visible(False)
-            ax.spines['bottom'].set_visible(False)
-            ax.axes.xaxis.set_visible(False)
-            ax.axes.yaxis.set_visible(False)
-            plt.savefig('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/clustering/MDS_kmeans_{}clusters.png'.format(clust+2))
+        # Load ROI labels
+        labels = np.loadtxt('/data1/Action_teresi/CCA/atlas/Schaefer_7N200_labels.txt', dtype=str)[ROIs_2keep]
+        hemispheres = np.array([0 if l[0]=='R' else 1 for l in labels])
+        labels_short = np.array([l[3:] for l in labels])
+        right = np.where(hemispheres==0)[0]
+        left = np.where(hemispheres)[0]
+
+        plt.figure(figsize=(9.6, 6.4))
+        scatt = plt.scatter(results_tsne[right,0], results_tsne[right,1], c=clusters_labels[clust, right], marker='o')
+        scattL = plt.scatter(results_tsne[left,0], results_tsne[left,1], c=clusters_labels[clust, left], marker='v')
+        plt.legend(*scattL.legend_elements(), loc='lower right', title='Clusters', ncol=2, frameon=False, borderpad=-1, labelspacing=0.1, borderaxespad=0.1, columnspacing=0.1, handletextpad=-0.5)
+        plt.title('Clustering ROIs on tSNE space from {}'.format(metric))
+        plt.suptitle('Silhouette score for {} clusters = {}'.format(clust+2, silhouette_avg[clust]))
+        ax = plt.gca()
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+
+        for i, txt in enumerate(labels_short):
+            ax.annotate(txt, (results_tsne[i,0]+0.5, results_tsne[i,1]+0.5), size='xx-small')
+
+        plt.savefig('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/clustering/tsne_{}_kmeans_{}clusters.png'.format(metric, clust+2))
+
+    # Save clustering res to nifti
+    clust_to_nifti = np.expand_dims(clusters_labels[clust], axis=0)
+    save_nifti(clust_to_nifti, 1, atlas_file, '/data1/Action_teresi/CCA/cca_results/group/CCA_tsne_euclidean_kmeans.nii.gz', ROIsmask=ROIs_2keep+1)
 
     # Load task models
     domains_list = ['space', 'movement', 'agent_objective', 'social_connectivity', 'emotion_expression', 'linguistic_predictiveness']
@@ -179,15 +252,9 @@ if __name__ == '__main__':
     n_predictors = np.sum([domains[d].shape[1] for d in domains_list])
     dom_combs = list(itertools.combinations(range(n_doms), 2))
     dom_combs_str = list(itertools.combinations(domains_list, 2))
-
-    # Evaluate atlas ROIs dimension
-    rois_nsubs, rois_nvoxs_subs = evaluate_nvoxels_rois(sub_list, atlas_file, n_predictors, print_opt=True, save=False)
-
-    # Load single subject results
-    results = np.load('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/group/CCA_res_allsubs{}_{}.npz'.format(suffix, atlas_file), allow_pickle=True)['results'][:,:,0,:]
-    results_group = np.mean(results, axis=0)
-
+    
     # Run t-tests
-    ts, pvals = ttest_domains(results, dom_combs, dom_combs_str, fdr_opt=True, save=True)
-
-    print('')
+    ts, pvals = ttest_rel_domains(results_vp, dom_combs, dom_combs_str, fdr_opt=True, save=True)
+    
+    # Save nifti
+    create_ttest_nifti(ts, 1-pvals, dom_combs, dom_combs_str)
