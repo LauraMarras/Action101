22731982a2802d383a30f78c50425d21cb0aa5e9
LauraMarras <laura.marras@imtlucca.it>
2024-10-21 13:52:36 +0000

    rename

--- code/analysis/analysis_pipeline_5_nonparametric_ttests.py
+++ code/analysis/analysis_pipeline_5_nonparametric_ttests.py
@@ -91,25 +91,15 @@ def save_nifti(results, briks, atlas_file='Schaefer200', savepath='', ROIsmask=[
 
     return
 
-def create_ttest_nifti(ts, pvals, dom_combs, dom_combs_str):
-        for c, comb in enumerate(dom_combs):
-            cres = np.expand_dims(np.vstack((ts[:,c], pvals[:,c])).T, axis=0)
-            path = '/data1/Action_teresi/CCA/cca_results/group/ttest/ttest_{}.nii.gz'.format(dom_combs_str[c][0][:5] + 'vs' + dom_combs_str[c][1][:5])
-            save_nifti(cres, 2, atlas_file, path, ROIs_2keep+1)
-
-        return
-
 if __name__ == '__main__': 
     
     # Set parameters
     condition = 'AV'
-    full_model_opt = True # full_model vs variance_partitioning (if False run Variance partitioning)
-
+   
     sub_lists = {'AV': np.array([12, 13, 14, 15, 16, 17, 18, 19, 22, 32]), 'vid': np.array([20, 21, 23, 24, 25, 26, 28, 29, 30, 31]), 'aud': np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 27])}
     sub_list = sub_lists[condition]
     n_subs = len(sub_list)
     global_path = '/home/laura.marras/Documents/Repositories/Action101/data/'
-
     atlas_file = 'Schaefer200'
 
     # Load group results
@@ -121,6 +111,8 @@ if __name__ == '__main__':
     domains = {d: np.loadtxt('/home/laura.marras/Documents/Repositories/Action101/data/models/domains/group_ds_conv_{}.csv'.format(d), delimiter=',', skiprows=1)[:, 1:] for d in domains_list}
     n_doms = len(domains.keys())
     n_predictors = np.sum([domains[d].shape[1] for d in domains_list])
+    dom_combs = list(itertools.product(range(n_doms), range(n_doms)))
+    dom_combs_str = list(itertools.product(domains_list,domains_list))
     dom_combs = list(itertools.combinations(range(n_doms), 2))
     dom_combs_str = list(itertools.combinations(domains_list, 2))
     
@@ -129,61 +121,41 @@ if __name__ == '__main__':
     if not os.path.exists(path):
                 os.makedirs(path)
 
-    ts, pvals = ttest_domains(results_singledoms, dom_combs, dom_combs_str, test2use='rel', fdr_opt=True, save=True, path=path)
+    # Get ranks
+    ranked_res =  np.argsort(np.argsort(results_singledoms, axis=-1))+1
+
+    maxwin = np.argmax((np.mean(ranked_res, axis=0)), axis=1)
+
+    # Init results mat
+    avg_differences = np.full((len(rois_sign), len(dom_combs)), np.nan)
+    wilcox_res = np.full((len(rois_sign), len(dom_combs), 2), np.nan)
+    ttest_res = np.full((len(rois_sign), len(dom_combs), 2), np.nan)
+
+    for r, roi in enumerate(rois_sign):
+        for d, comb in enumerate(dom_combs):
+              
+            dom1 = ranked_res[:,r,comb[0]]
+            dom2 = ranked_res[:,r,comb[1]]
+
+            avg_differences[r, d] = np.mean(dom1) - np.mean(dom2)
+
+            stat, pval = wilcoxon(dom1, dom2)
+            wilcox_res[r, d] = stat, pval
+
+
+            statt, pvalt = ttest_rel(dom1, dom2)
+            ttest_res[r, d] = statt, pvalt
+
+        
+        
+    # Plot
+    fig, ax = plt.subplots()
+    sns.heatmap(avg_differences, ax=ax, square=True, annot=True, xticklabels=list(domains.keys()), yticklabels=list(domains.keys()), cmap='rocket_r', cbar_kws=dict(pad=0.01,shrink=0.9))
     
-    # Save nifti
-    # create_ttest_nifti(ts, 1-pvals, dom_combs, dom_combs_str)
+    # Labels and layout
+    plt.tight_layout()
 
-    ts = np.load('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/ttest/ttest_rel_cca.npz', allow_pickle=True)['tstat']
-    pvals = np.load('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/ttest/ttest_rel_cca.npz', allow_pickle=True)['pvals']
+    # Save
+    plt.savefig(path + 'ranked_test_domains_roi{}.png'.format(roi))
 
-    doms_color = {'space': '#ff595e', 'movement':'#ff924c', 'agent_objective':'#ffca3a', 'social_connectivity':'#8ac926', 'emotion_expression':'#1982c4', 'linguistic_predictiveness':'#6a4c93'}
-    
-    for c, comb in enumerate(dom_combs_str):
-        
-        cmap = LinearSegmentedColormap.from_list('mycmap', [doms_color[comb[0]], doms_color[comb[1]], '#adb5bd'], N=3)
-    
-        color_coding = np.array([0 if t>0  else 1 for t in ts[:,c]])
-        color_coding[np.where(pvals[:,c]>0.05)] = '2'
-        
-        point_labels = np.array(['{}'.format(comb[0]) if t >0 else '{}'.format(comb[1]) for t in ts[:,c]])
-        point_labels[np.where(pvals[:,c]>0.05)] = 'ns'
-
-        # Load ROI labels
-        labels = np.loadtxt('/data1/Action_teresi/CCA/atlas/Schaefer_7N200_labels.txt', dtype=str)[ROIs_2keep]
-        hemispheres = np.array([0 if l[0]=='R' else 1 for l in labels])
-        labels_short = np.array([l[3:] for l in labels])
-        right = np.where(hemispheres==0)[0]
-        left = np.where(hemispheres)[0]
-
-        plt.figure(figsize=(9.6, 6.4))
-        scatt = plt.scatter(results_tsne[right,0], results_tsne[right,1], c=color_coding[right], cmap=cmap, marker='o')
-        scattL = plt.scatter(results_tsne[left,0], results_tsne[left,1], c=color_coding[left], cmap=cmap, marker='v')
-        
-        # Create legend
-
-        legend_elements = [Line2D([0], [0], color='w', markerfacecolor=doms_color[comb[0]], marker='o', label=comb[0]),
-                            Line2D([0], [0], color='w', markerfacecolor=doms_color[comb[1]], marker='o', label=comb[1]),
-                            Line2D([0], [0], color='w', markerfacecolor='#adb5bd', marker='o', label='ns'),
-                            Line2D([0], [0], color='w', markerfacecolor='k', marker='o', label='right'),
-                            Line2D([0], [0], color='w', markerfacecolor='k', marker='v', label='left')]
-                   
-        plt.legend(handles=legend_elements, loc='upper left', ncol=1, frameon=False, borderpad=-1, labelspacing=0.1, borderaxespad=0.1, columnspacing=0.1, handletextpad=-0.5)
-                
-        
-        plt.title('T-Test on tSNE space from {}\n{}'.format('functional connectivity', comb[0]+' vs '+comb[1]))
-        ax = plt.gca()
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.spines['left'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
-        ax.axes.xaxis.set_visible(False)
-        ax.axes.yaxis.set_visible(False)
-
-        for i, txt in enumerate(labels_short):
-            ax.annotate(txt, (results_tsne[i,0]+0.5, results_tsne[i,1]+0.5), size='xx-small')
-
-        plt.savefig('/home/laura.marras/Documents/Repositories/Action101/data/cca_results/clustering/ttest_{}_vs_{}.png'.format(comb[0], comb[1]))
-
-
-        print('')
+    print('d')
