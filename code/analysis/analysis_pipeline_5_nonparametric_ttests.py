eabcc07f206d3797529ad7b779f8bcb58775af15
LauraMarras <laura.marras@imtlucca.it>
2024-10-23 16:46:48 +0000

    finish script

--- code/analysis/analysis_pipeline_5_nonparametric_ttests.py
+++ code/analysis/analysis_pipeline_5_nonparametric_ttests.py
@@ -3,11 +3,12 @@ import numpy as np
 from scipy.stats import false_discovery_control as fdr
 from scipy.stats import wilcoxon, friedmanchisquare
 import itertools
+import pandas as pd
 
 if __name__ == '__main__': 
     
     # Set parameters
-    condition = 'AV'
+    condition = 'aud'
    
     sub_lists = {'AV': np.array([12, 13, 14, 15, 16, 17, 18, 19, 22, 32]), 'vid': np.array([20, 21, 23, 24, 25, 26, 28, 29, 30, 31]), 'aud': np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 27])}
     sub_list = sub_lists[condition]
@@ -16,8 +17,10 @@ if __name__ == '__main__':
     atlas_file = 'Schaefer200'
 
     # Load group results
-    results_singledoms = np.load('{}cca_results/{}/group/single_doms/CCA_R2_allsubs_singledoms.npz'.format(global_path, condition), allow_pickle=True)['results_subs_sd']
+    rois_sign_AV = np.load('{}cca_results/AV/group/single_doms/CCA_R2_allsubs_singledoms.npz'.format(global_path), allow_pickle=True)['rois_list']
     rois_sign = np.load('{}cca_results/{}/group/single_doms/CCA_R2_allsubs_singledoms.npz'.format(global_path, condition), allow_pickle=True)['rois_list']
+    rois_indx = np.where(np.isin(rois_sign_AV, rois_sign))[0]
+    results_singledoms = np.load('{}cca_results/{}/group/single_doms/CCA_R2_allsubs_singledoms.npz'.format(global_path, condition), allow_pickle=True)['results_subs_sd'][:,rois_indx,:]
 
     # Load task models
     domains_list = ['space', 'movement', 'agent_objective', 'social_connectivity', 'emotion_expression', 'linguistic_predictiveness']
@@ -41,37 +44,52 @@ if __name__ == '__main__':
 
     # Correct for multiple comparisons
     friedman_qvals = fdr(friedman_pvals)
-
+    correction_fried='_fdr'
+    
+    if len(np.where(friedman_qvals<0.05)[0]) == 0:
+        friedman_qvals = friedman_pvals
+        print('no significant qvalues in the Friedman test, considering pvalues')
+        correction_fried=''
+    
     # Run pairwise comparisons with wilcoxon for significant ROIs
     idx_fried_sign = np.where(friedman_qvals<0.05)[0]
     rois_fried_sign = rois_sign[idx_fried_sign]
-    
+
     # Initialize results mat
     wilcox_stats = np.full((len(rois_fried_sign), len(dom_combs)), np.nan)
     wilcox_pvals = np.full((len(rois_fried_sign), len(dom_combs)), np.nan)
     wilcox_doms = np.full((len(rois_fried_sign), len(dom_combs)), np.nan)
     
-
     # Iterate over doms combinations
     for d, comb in enumerate(dom_combs):
         dom1 = rois_ranks[:, idx_fried_sign, comb[0]]
         dom2 = rois_ranks[:, idx_fried_sign, comb[1]]
 
         wilcox_stats[:, d], wilcox_pvals[:, d] = wilcoxon(dom1, dom2, axis=0)
-        wilcox_doms[:, d] = np.array([comb[i] for i in (np.median(dom1-dom2, axis=0)<0)*1])
+        wilcox_doms[:, d] = (np.median(dom1-dom2, axis=0)<0)*1
 
     wilcox_doms = wilcox_doms.astype(int)
     
     # Correct for multiple comparisons
-    wilcox_qvals = fdr(wilcox_pvals)
+    if len(wilcox_pvals) > 0:
+        wilcox_qvals = fdr(wilcox_pvals)
 
-    rois_wilcox_sign = rois_fried_sign[np.where(wilcox_qvals<0.05)[0]]
-    combs_wilcox_sign = np.array(dom_combs_str)[np.where(wilcox_qvals<0.05)[1]]
-    windom_wilcox_sign = combs_wilcox_sign[wilcox_doms[np.where(wilcox_qvals<0.05)]]
+        if len(np.where(wilcox_qvals<0.05)[0]) == 0:
+            wilcox_qvals = wilcox_pvals
+            print('no significant qvalues in the Wilcoxon test, considering pvalues')
+            correction_wilc=''
+        
+        else:
+            correction_wilc='_fdr'
 
-    for r, roi in enumerate(rois_wilcox_sign):
-        print(roi)
-        print(combs_wilcox_sign[r])
-        print(windom_wilcox_sign[r])
+        # Save significant results to csv file
+        rois_wilcox_sign = rois_fried_sign[np.where(wilcox_qvals<0.05)[0]]
+        combs_wilcox_sign = np.array(dom_combs_str)[np.where(wilcox_qvals<0.05)[1]]
+        windom_wilcox_sign = np.array([[dom_combs_str[c,comb] for c, comb in enumerate(wilcox_doms[r,:])] for r in range(wilcox_doms.shape[0])])[np.where(wilcox_qvals<0.05)]
 
-    print('d')
+        results = pd.DataFrame((rois_wilcox_sign, combs_wilcox_sign, windom_wilcox_sign)).T
+        results.to_csv('{}{}_friedman{}_wilcoxon{}.csv'.format(path, condition, correction_fried, correction_wilc), header=['rois', 'comparison', 'domain'], index=False)
+
+    else:
+        print('no significant pvalues in the Wilcoxon test')
+    
