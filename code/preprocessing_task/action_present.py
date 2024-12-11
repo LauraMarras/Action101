f4cd8e2b86816fc28e17ea9be13f8470c5c72759
LauraMarras <laura.marras@imtlucca.it>
2024-12-11 20:33:40 +0000

    move action present to separate script

---
+++ code/preprocessing_task/action_present.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import dice
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.gridspec as gridspec
+from itertools import combinations
+
+def config_plt(textsize=8):
+
+    plt.rc('font', family='Arial')
+    plt.rc('axes', titlesize=12)
+    plt.rc('axes', labelsize=textsize)
+    plt.rc('xtick', labelsize=textsize)
+    plt.rc('ytick', labelsize=textsize)
+
+    plt.rc('legend', fontsize=textsize)
+    plt.rc('legend', loc='best')
+    plt.rc('legend', frameon=False)
+
+    plt.rc('grid', linewidth=0.5)
+    plt.rc('axes', linewidth=0.5)
+    plt.rc('xtick.major', width=0.5)
+    plt.rc('ytick.major', width=0.5)
+
+def plot_actionpres(mat, bar_values, raters, path):
+
+    fig = plt.figure(figsize=(12, 3))
+    gs = gridspec.GridSpec(1, 2, width_ratios=[7,1], wspace=50)
+
+    ax = fig.add_subplot(gs[0])
+    ax_bar = fig.add_subplot(gs[1])
+
+    ax.imshow(mat, aspect='auto', cmap='Purples', interpolation='nearest')
+
+    # Set axis labels
+    ax.set_yticks([*range(len(raters))])
+    ax.set_yticklabels(raters)
+    ax.set_ylabel('Raters')
+
+    ax.axhline(0.5, color='w', linewidth=2)
+    ax.axhline(1.5, color='w', linewidth=2)
+
+    run_cuts = np.array([0, 268,  493,  813, 1138, 1374, 1614])
+    for r in run_cuts:
+        ax.axvline(r, color='r', linewidth=2)
+
+    ax.set_xlim([-1.5,1615.5])
+    ax.set_xlabel('Time (s)')
+
+    ax2 = ax.secondary_xaxis('top')
+    ax2.set_xticks(run_cuts[:-1])
+    ax2.set_xticklabels(['Run1', 'Run2', 'Run3', 'Run4', 'Run5', 'Run6'])
+
+    ax.spines['right'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+
+    # Add legend
+    legend_elements = [
+    mpatches.Patch(facecolor=plt.cm.Purples(0.99), edgecolor='black', label='present'),
+    mpatches.Patch(facecolor=plt.cm.Purples(0), edgecolor='black', label='not present')
+    ]
+    ax.legend(handles=legend_elements, loc='upper left', title="Action", frameon=False, bbox_to_anchor=(1,1))
+
+    # Add barplot
+    xbar = [0,0.5,1]
+    ax_bar.bar(xbar, bar_values, width=0.5, color=['#058c42', '#ec4d37', '#6f2dbd'], alpha=0.85)
+    ax_bar.set_ylabel('Dice similarity')
+    ax_bar.set_xlabel('Raters pair')
+
+    ax_bar.set_xticks(xbar)
+    ax_bar.set_xticklabels(['','',''])
+
+    ax_bar.spines['right'].set_visible(False)
+    ax_bar.spines['top'].set_visible(False)
+
+    labelss = ['AI vs LM', 'AI vs LT', 'LM vs LT']
+    legend_elements2 = [
+    mpatches.Patch(facecolor='#058c42', label='AI vs LM'), mpatches.Patch(facecolor='#ec4d37', label='AI vs LT'), mpatches.Patch(facecolor='#6f2dbd', label='LM vs LT'),
+    ]
+
+    ax_bar.legend(handles=legend_elements2, loc='upper center', title="Raters pair", frameon=False, bbox_to_anchor=(-1.3, 0.5))
+
+    gs.tight_layout(fig)
+    plt.savefig(path + 'inter-rater_action_present.png')
+
+if __name__ == '__main__':
+
+    path = '/home/laura.marras/Documents/Repositories/Action101/data/models/inter_raters_agreement/'
+    raters = ['AI', 'LM', 'LT']
+    pairings = list(combinations(raters, 2))
+
+    # Load single tagger action_present column
+    ap_dict = {r: pd.read_csv(path + '{}_ds.csv'.format(r), sep=',')['action_present'].to_numpy() for r in raters}
+    
+    # Initialize results matrix
+    dice_res = np.full((len(pairings)), np.nan)
+
+    for p, pair in enumerate(pairings):
+        dice_res[p] = 1-dice(ap_dict[pair[0]], ap_dict[pair[1]])
+        
+    # Plot 
+    ap_mat = np.vstack(list(ap_dict.values()))
+    plot_actionpres(ap_mat, dice_res, raters, path)
--- code/preprocessing_task/inter_rater_agreement.py
+++ code/preprocessing_task/inter_rater_agreement.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
-from utils.similarity_measures import canonical_correlation, linear_cka, gram_linear, center_gram, cka
+from utils.similarity_measures import linear_cka
 from utils.permutation_schema_func import permutation_schema
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-from sklearn.metrics import jaccard_score
-from scipy.spatial.distance import dice
-import matplotlib.gridspec as gridspec
+from itertools import combinations
 
 def config_plt(textsize=8):
 
@@ -24,112 +22,6 @@ def config_plt(textsize=8):
     plt.rc('axes', linewidth=0.5)
     plt.rc('xtick.major', width=0.5)
     plt.rc('ytick.major', width=0.5)
-
-def plot_actionpres(ai,lm,lt, bar_values, path):
-
-    fig = plt.figure(figsize=(12, 3))
-    gs = gridspec.GridSpec(1, 2, width_ratios=[7,1], wspace=50)
-
-    ax = fig.add_subplot(gs[0])
-    ax_bar = fig.add_subplot(gs[1])
-
-    ax.imshow(np.vstack((ai,lm,lt)), aspect='auto', cmap='Purples', interpolation='nearest')
-
-    # Set axis labels
-    ax.set_yticks([0, 1, 2])
-    ax.set_yticklabels(['AI', 'LM', 'LT'])
-    ax.set_ylabel('Raters')
-
-    ax.axhline(0.5, color='w', linewidth=2)
-    ax.axhline(1.5, color='w', linewidth=2)
-
-    run_cuts = np.array([0, 268,  493,  813, 1138, 1374, 1614])
-    for r in run_cuts:
-        ax.axvline(r, color='r', linewidth=2)
-
-    ax.set_xlim([-1.5,1615.5])
-    ax.set_xlabel('Time (s)')
-
-    ax2 = ax.secondary_xaxis('top')
-    ax2.set_xticks(run_cuts[:-1])
-    ax2.set_xticklabels(['Run1', 'Run2', 'Run3', 'Run4', 'Run5', 'Run6'])
-
-    ax.spines['right'].set_visible(False)
-    ax2.spines['right'].set_visible(False)
-
-    # Add legend
-    legend_elements = [
-    mpatches.Patch(facecolor=plt.cm.Purples(0.99), edgecolor='black', label='present'),
-    mpatches.Patch(facecolor=plt.cm.Purples(0), edgecolor='black', label='not present')
-    ]
-    ax.legend(handles=legend_elements, loc='upper left', title="Action", frameon=False, bbox_to_anchor=(1,1))
-
-    # Add barplot
-    xbar = [0,0.5,1]
-    ax_bar.bar(xbar, bar_values, width=0.5, color=['#058c42', '#ec4d37', '#6f2dbd'], alpha=0.85)
-    ax_bar.set_ylabel('Dice similarity')
-    ax_bar.set_xlabel('Raters pair')
-
-    ax_bar.set_xticks(xbar)
-    ax_bar.set_xticklabels(['','',''])
-
-    ax_bar.spines['right'].set_visible(False)
-    ax_bar.spines['top'].set_visible(False)
-
-    labelss = ['AI vs LM', 'AI vs LT', 'LM vs LT']
-    legend_elements2 = [
-    mpatches.Patch(facecolor='#058c42', label='AI vs LM'), mpatches.Patch(facecolor='#ec4d37', label='AI vs LT'), mpatches.Patch(facecolor='#6f2dbd', label='LM vs LT'),
-    ]
-
-    ax_bar.legend(handles=legend_elements2, loc='upper center', title="Raters pair", frameon=False, bbox_to_anchor=(-1.3, 0.5))
-
-    gs.tight_layout(fig)
-    plt.savefig(path + 'inter-rater_action_present.png')
-
-def plot_corrs(results, results2, domains, measure, path):
-   
-    # Parameters
-    n_rater_pairs = results.shape[0]
-    n_domains = results.shape[1]
-    rater_pairs = ['AI vs LM', 'AI vs LT', 'LM vs LT']
-    bar_width = 0.2
-
-    x = np.arange(n_domains)
-
-    # Plot
-    fig, ax = plt.subplots(figsize=(10, 7.5))
-    colors = ['#058c42', '#ec4d37', '#6f2dbd']
-
-    for i in range(n_rater_pairs):
-        ax.bar(x + i * bar_width, results[i], bar_width, label=rater_pairs[i],  color=colors[i], alpha=0.85)
-        
-        ax.bar(x[:-1] + i * bar_width, results2[i,:-1], bar_width, color=colors[i], hatch='////', edgecolor='black')
-
-    ax_right = ax.twinx()
-    ax_right.set_ylim(0,1)
-    ax_right.spines['top'].set_visible(False)
-    ax_right.set_ylabel('Dice similarity')
-
-
-    # Labels and layout
-    ax.set_xlabel('Domains')
-    ax.set_ylabel('{}'.format(measure))
-    ax.set_title('{} between raters for each domain'.format(measure))
-    ax.set_xticks(x + bar_width)
-    ax.set_xticklabels(domains, rotation=45)
-    ax.set_ylim(0,1)
-
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    plt.tight_layout()
-
-    # Legend
-    solid_patches = [mpatches.Patch(color=colors[i], label=f"{rater_pairs[i]}") for i in range(n_rater_pairs)]
-    hatch_patches = [mpatches.Patch(facecolor='w', hatch='////', edgecolor='black', label='Shuffled'), mpatches.Patch(facecolor='k', edgecolor='black', label='Real')]
-    ax.legend(handles=solid_patches + hatch_patches, title='Rater Pairs', frameon=False)
-
-    # Save
-    plt.savefig(path + 'inter-rater_{}.png'.format(measure))
 
 def plot_corrs_means(results, results2, res_std, res2_std, domains, measure, path):
    
@@ -172,81 +64,76 @@ def plot_corrs_means(results, results2, res_std, res2_std, domains, measure, pat
 
 if __name__ == '__main__':
 
-    # Define parameters
-    taggers = ['LM', 'AI', 'LT']
     path = '/home/laura.marras/Documents/Repositories/Action101/data/models/inter_raters_agreement/'
+    plot = False
     
-    domains = {
-    'space':                        ['context_0', 'context_1', 'context_2', 'inter_scale'],
-
-    'movement':                     ['eff_visibility', 'main_effector_0', 'main_effector_1', 'main_effector_2', 
-                                    'main_effector_3', 'main_effector_4', 'main_effector_5', 'main_effector_6',
-                                    'main_effector_7', 'main_effector_8', 'main_effector_9', 'main_effector_10'],
-
-    'agent_objective':              ['target_0','target_1', 'agent_H_NH', 'tool_mediated', 'transitivity', 'touch_2'],
-
-    'social_connectivity':          ['sociality', 'touch_1', 'target_2', 'target_3', 'multi_ag_vs_jointact_1', 
-                                     'multi_ag_vs_jointact_2', 'ToM', 'people_present'],
-
-    'emotion_expression':           ['EBL', 'EIA', 'gesticolare', 'simbolic_gestures'],
-
-    'linguistic_predictiveness':    ['durativity', 'telicity', 'iterativity', 'dinamicity'],
-
-    'full':                         ['context_0', 'context_1', 'context_2', 'inter_scale', 
-                                    
-                                    'eff_visibility', 'main_effector_0', 'main_effector_1', 'main_effector_2', 
-                                    'main_effector_3', 'main_effector_4', 'main_effector_5', 'main_effector_6',
-                                    'main_effector_7', 'main_effector_8', 'main_effector_9', 'main_effector_10', 
-                                    
-                                    'target_0','target_1', 'agent_H_NH', 'tool_mediated', 'transitivity', 'touch_2', 
-                                    
-                                    'sociality', 'touch_1', 'target_2', 'target_3', 'multi_ag_vs_jointact_1', 
-                                    'multi_ag_vs_jointact_2', 'ToM', 'people_present',
-                                    
-                                    'EBL', 'EIA', 'gesticolare', 'simbolic_gestures',
-                                    
-                                    'durativity', 'telicity', 'iterativity', 'dinamicity']
-    } # 'action_present':               'action_present'
+    # Load CKA results
+    try:
+        res = np.load('{}inter_rater_dbCKA_permut.npy'.format(path))
     
-    # Load single tagger tagging
-    AI = pd.read_csv(path + '{}_ds.csv'.format('AI'), sep=',')
-    LM = pd.read_csv(path + '{}_ds.csv'.format('LM'), sep=',')
-    LT = pd.read_csv(path + '{}_ds.csv'.format('LT'), sep=',')
-    
-    # Create perm schema
-    max_cols = np.max([len(v) for v in domains.values()])
-    t_points = AI.shape[0]
-    n_perms = 1000
-    perm_schema = permutation_schema(t_points, n_perms)
-    
-    # Initialize results matrix
-    res = np.zeros((len(taggers), len(domains), n_perms+1))
-    
-    # Calculate correlation between raters for each domain and permutation
-    for d, dom in enumerate(domains.keys()):
-        ai = AI[domains[dom]].to_numpy()
-        lm = LM[domains[dom]].to_numpy()
-        lt = LT[domains[dom]].to_numpy()
+    except FileNotFoundError:
         
-        for p in range(perm_schema.shape[1]):
-            order = perm_schema[:,p]
+        raters = ['LM', 'AI', 'LT']
+        pairings = list(combinations(raters, 2))
+        domains = {
+        'space':                        ['context_0', 'context_1', 'context_2', 'inter_scale'],
 
-            # For all domains get CKA
-            if dom != 'action_present':
-                res[0, d, p] = linear_cka(ai, lm[order, :], debiasing=True)
-                res[1, d, p] = linear_cka(ai, lt[order, :], debiasing=True)
-                res[2, d, p] = linear_cka(lm, lt[order, :], debiasing=True)
+        'movement':                     ['eff_visibility', 'main_effector_0', 'main_effector_1', 'main_effector_2', 
+                                        'main_effector_3', 'main_effector_4', 'main_effector_5', 'main_effector_6',
+                                        'main_effector_7', 'main_effector_8', 'main_effector_9', 'main_effector_10'],
+
+        'agent_objective':              ['target_0','target_1', 'agent_H_NH', 'tool_mediated', 'transitivity', 'touch_2'],
+
+        'social_connectivity':          ['sociality', 'touch_1', 'target_2', 'target_3', 'multi_ag_vs_jointact_1', 
+                                        'multi_ag_vs_jointact_2', 'ToM', 'people_present'],
+
+        'emotion_expression':           ['EBL', 'EIA', 'gesticolare', 'simbolic_gestures'],
+
+        'linguistic_predictiveness':    ['durativity', 'telicity', 'iterativity', 'dinamicity'],
+
+        'full':                         ['context_0', 'context_1', 'context_2', 'inter_scale', 
+                                        
+                                        'eff_visibility', 'main_effector_0', 'main_effector_1', 'main_effector_2', 
+                                        'main_effector_3', 'main_effector_4', 'main_effector_5', 'main_effector_6',
+                                        'main_effector_7', 'main_effector_8', 'main_effector_9', 'main_effector_10', 
+                                        
+                                        'target_0','target_1', 'agent_H_NH', 'tool_mediated', 'transitivity', 'touch_2', 
+                                        
+                                        'sociality', 'touch_1', 'target_2', 'target_3', 'multi_ag_vs_jointact_1', 
+                                        'multi_ag_vs_jointact_2', 'ToM', 'people_present',
+                                        
+                                        'EBL', 'EIA', 'gesticolare', 'simbolic_gestures',
+                                        
+                                        'durativity', 'telicity', 'iterativity', 'dinamicity']
+        } # 'action_present':               'action_present'
+        
+        # Load single tagger tagging
+        tagging_dict = {r: pd.read_csv(path + '{}_ds.csv'.format(r), sep=',') for r in raters}
+        
+        # Create perm schema
+        max_cols = np.max([len(v) for v in domains.values()])
+        t_points = tagging_dict[raters[0]].shape[0]
+        n_perms = 1000
+        perm_schema = permutation_schema(t_points, n_perms)
+        
+        # Initialize results matrix
+        res = np.zeros((len(raters), len(domains), n_perms+1))
+        
+        # Calculate correlation between raters for each domain and permutation
+        for d, dom in enumerate(domains.keys()):
             
-            # For Action present get dice similarity (do not permute - sparcity too low)
-            else:
-                if p==0:
-                    res[0, d, p] = 1-dice(ai, lm[order])
-                    res[1, d, p] = 1-dice(ai, lt[order])
-                    res[2, d, p] = 1-dice(lm, lt[order])
-                
-                else:
-                    continue
+            for p in range(perm_schema.shape[1]):
+                order = perm_schema[:,p]
 
+                # For all domains get CKA
+                for pr, pair in enumerate(pairings):
+                    t1 = tagging_dict[pair[0]][domains[dom]].to_numpy()
+                    t2 = tagging_dict[pair[1]][domains[dom]].to_numpy()
+                    
+                    res[pr, d, p] = linear_cka(t1, t2[order, :], debiasing=True)
+
+        np.save('{}inter_rater_dbCKA_permut.npy'.format(path), res)
+    
     # Get null distribution 99th percentile
     res_null = np.percentile(res[:, :, 1:], q=99, axis=-1)
     res_real = res[:, :, 0]
@@ -258,15 +145,8 @@ if __name__ == '__main__':
     res_null_std = np.std(res_null, axis=0)
 
     # Plot
-    config_plt()
-    domains_labels = ['space', 'effector', 'agent-object', 'social', 'emotion', 'linguistic', 'full']
-    plot_path = '/home/laura.marras/Documents/Repositories/Action101/data/plots_final/'
-    plot_corrs_means(res_mean, res_null_mean, res_std, res_null_std, domains_labels, 'Inter-rater agreement (CKA)', plot_path)
-
-
-    act_ai = AI['action_present'].to_numpy()
-    act_lm = LM['action_present'].to_numpy()
-    act_lt = LT['action_present'].to_numpy()
-    plot_actionpres(act_ai, act_lm, act_lt, path)
-
-    print('d')
+    if plot:
+        config_plt()
+        domains_labels = ['space', 'effector', 'agent-object', 'social', 'emotion', 'linguistic', 'full']
+        plot_path = '/home/laura.marras/Documents/Repositories/Action101/data/plots_final/'
+        plot_corrs_means(res_mean, res_null_mean, res_std, res_null_std, domains_labels, 'Inter-rater agreement (CKA)', plot_path)
