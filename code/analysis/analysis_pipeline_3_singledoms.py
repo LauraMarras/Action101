3f3c1ddfad4ef593ac9270f9b9a617cde9a48334
LauraMarras <laura.marras@imtlucca.it>
2025-01-23 11:09:23 +0000

    Get descriptives

--- code/analysis/analysis_pipeline_3_singledoms.py
+++ code/analysis/analysis_pipeline_3_singledoms.py
@@ -1,33 +1,6 @@
 import os
 import numpy as np
 from nilearn import image
-from scipy.stats import false_discovery_control as fdr
-from scipy.stats import ttest_ind
-
-def ttest_domains(results, dom_combs, dom_combs_str, fdr_opt=True, save=False):
-    
-    # Initialize results matrices
-    ts = np.empty((results.shape[1], len(dom_combs)))
-    pvals = np.empty((results.shape[1], len(dom_combs)))
-
-    # Run t-test for each ROI and each domains pairing
-    for roi in range(results.shape[1]):  
-        for comb in dom_combs:
-            d1 = results[:, roi, comb[0]]
-            d2 = results[:, roi, comb[1]]
-            t, pval = ttest_ind(d1, d2)
-            ts[roi,comb] = t 
-            pvals[roi,comb] = pval
-    
-    # FDR correction for multiple comparisons
-    if fdr_opt:
-        pvals = np.reshape(fdr(np.ravel(pvals)), pvals.shape)
-
-    # Save results
-    if save:
-        np.savez('/data1/Action_teresi/CCA/ttest_cca', tstat=ts, pvals=pvals, domains_contrasts=dom_combs_str, FDRcorrection=fdr_opt)
-
-    return ts, pvals
 
 def save_nifti(results, briks, atlas_file='Schaefer200', savepath='', ROIsmask=[]):
     
@@ -55,46 +28,97 @@ def save_nifti(results, briks, atlas_file='Schaefer200', savepath='', ROIsmask=[
 if __name__ == '__main__': 
     
     # Set options and parameters
-    condition = 'aud'
-    save = True
-    save_nifti_opt = True
+    conditions = ['AV', 'vid', 'aud']
+    save = False
+    save_nifti_opt = False
     atlas_file = 'Schaefer200'
     alpha = 0.05
     maxT = True
 
     global_path = '/home/laura.marras/Documents/Repositories/Action101/data/'
+    domains_list = np.array(['space', 'effector', 'agent & object', 'social', 'emotion', 'linguistic'])
 
-    domains_list = ['space', 'movement', 'agent_objective', 'social_connectivity', 'emotion_expression', 'linguistic_predictiveness']
+    for condition in conditions:
 
-    # Filter ROIs with significant R2 for full model
-    pvals_group_fm = np.load('{}cca_results/{}/group/fullmodel/CCA_R2_pvals{}_group_{}.npz'.format(global_path, condition, '_maxT' if maxT else '', atlas_file), allow_pickle=True)['pvals_group']
-    ROIs_sign_idx = np.unique(np.where(np.squeeze(pvals_group_fm) < alpha)[0])
-    ROIs_sign = np.load('{}cca_results/{}/group/fullmodel/CCA_R2_pvals{}_group_{}.npz'.format(global_path, condition, '_maxT' if maxT else '', atlas_file), allow_pickle=True)['rois_list'][ROIs_sign_idx]
+        # Filter ROIs with significant R2 for full model
+        pvals_group_fm, results_group_fm, rois_list = np.load('{}cca_results/{}/group/fullmodel/CCA_R2_pvals{}_group_{}.npz'.format(global_path, condition, '_maxT' if maxT else '', atlas_file), allow_pickle=True).values()
+        ROIs_sign_idx = np.unique(np.where(np.squeeze(pvals_group_fm) < alpha)[0])
+        ROIs_sign = rois_list[ROIs_sign_idx]
 
-    # Save list of significant ROIs
-    np.savetxt('{}cca_results/{}/group/fullmodel/significantROIs_{}.txt'.format(global_path, condition, condition), ROIs_sign)
+        # Load atlas labels and coords and filter for only tested ROIs
+        roi_labels = np.loadtxt('/data1/Action_teresi/CCA/atlas/Schaefer_7N200_labels.txt', dtype=str)[rois_list-1]
+        roi_coords = np.loadtxt('/data1/Action_teresi/CCA/atlas/Schaefer200_CdM.txt').round().astype(int)[rois_list,:]*np.array([-1, -1, 1]) # Convert from RAI to LPI
 
-    # Load R2 from full model and variance partitioning, select first perm (true R), and significative ROIs 
-    results_vp = np.load('{}cca_results/{}/group/variancepart/CCA_R2_allsubs_{}.npz'.format(global_path, condition, atlas_file), allow_pickle=True)['results'][:, ROIs_sign_idx, 0, :]
-    results_fm = np.load('{}cca_results/{}/group/fullmodel/CCA_R2_pvals_allsubs_{}.npz'.format(global_path, condition, atlas_file), allow_pickle=True)['results'][:, ROIs_sign_idx, 0, :]
-   
-    # Get R2 for single domain (subtract shuffled models R2 from full model R2)
-    res_dom = np.squeeze(results_fm - results_vp)
+        # Get descriptives
+        r2_avg_allrois = np.mean(results_group_fm).round(4)
+        r2_std_allrois = np.std(results_group_fm).round(4)
+        r2_max = np.max(results_group_fm).round(4)
+        r2_max_roilab = roi_labels[np.argmax(results_group_fm)]
+        r2_max_roi = rois_list[np.argmax(results_group_fm)]
+        r2_max_coords = roi_coords[np.argmax(results_group_fm)]
+        r2_avg_signrois = np.mean(results_group_fm[ROIs_sign_idx]).round(4)
+        r2_std_signrois = np.std(results_group_fm[ROIs_sign_idx]).round(4)
 
-    # Get mean across subs and concatenate to main matrix
-    res_dom_mean = np.mean(res_dom, axis=0)
-    res_dom_final = np.concatenate((res_dom, np.expand_dims(res_dom_mean, axis=0)), axis=0)
-
-    # Save results
-    if save:
-        path = '{}/cca_results/{}/group/single_doms/'.format(global_path, condition)
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        np.savez('{}CCA_R2_group_singledoms'.format(path), results_group_sd=res_dom_mean, rois_list=ROIs_sign)
-        np.savez('{}CCA_R2_allsubs_singledoms'.format(path), results_subs_sd=res_dom, rois_list=ROIs_sign)
+        results_text = '''
+        Results full model {}:
+        Average R2 across all tested rois (N={}) is {} ± {}
+        Highest R2 is {}, in ROI_{} {} at x={}, y={}, z={}; LPI
+        Average R2 across significant rois (N={}) is {} ± {}
+        '''.format(condition,
+                   len(rois_list), r2_avg_allrois, r2_std_allrois,
+                   r2_max, r2_max_roi, r2_max_roilab, r2_max_coords[0], r2_max_coords[1], r2_max_coords[2],
+                   len(ROIs_sign), r2_avg_signrois, r2_std_signrois)
+        
+        # Load R2 from full model and variance partitioning, select first perm (true R), and significative ROIs
+        results_vp = np.load('{}cca_results/{}/group/variancepart/CCA_R2_allsubs_{}.npz'.format(global_path, condition, atlas_file), allow_pickle=True)['results'][:, ROIs_sign_idx, 0, :]
+        results_fm = np.load('{}cca_results/{}/group/fullmodel/CCA_R2_pvals_allsubs_{}.npz'.format(global_path, condition, atlas_file), allow_pickle=True)['results'][:, ROIs_sign_idx, 0, :]
     
-    # Save nifti
-    if save_nifti_opt:
-        for dom in range(res_dom.shape[-1]):
-            save_nifti(res_dom_final[:,:,dom], res_dom_final.shape[0], atlas_file, '{}CCA_R2_{}_{}.nii'.format(path, domains_list[dom], atlas_file), ROIs_sign)
+        # Get R2 for single domain (subtract shuffled models R2 from full model R2)
+        res_dom = np.squeeze(results_fm - results_vp)
+
+        # Get mean across subs and concatenate to main matrix
+        res_dom_mean = np.mean(res_dom, axis=0)
+        res_dom_final = np.concatenate((res_dom, np.expand_dims(res_dom_mean, axis=0)), axis=0)
+
+        # Get descriptives
+        r2_max_doms = np.max(res_dom_mean, axis=0).round(4)
+        r2_max_doms_roi = rois_list[ROIs_sign_idx][np.argmax(res_dom_mean, axis=0)]
+        r2_max_doms_roilab = roi_labels[ROIs_sign_idx][np.argmax(res_dom_mean, axis=0)]
+        r2_max_doms_coords = roi_coords[ROIs_sign_idx][np.argmax(res_dom_mean, axis=0)]
+        r2_avg_doms = np.mean(res_dom_mean, axis=0).round(4)
+        r2_std_doms = np.std(res_dom_mean, axis=0).round(4)
+        percent_fm = ((r2_avg_doms*100)/r2_avg_signrois).round(1)
+        percent_shared = (((r2_avg_signrois-np.sum(r2_avg_doms))*100)/r2_avg_signrois).round(1)
+        order = np.argsort(r2_avg_doms)[::-1]
+
+        for d, dom in enumerate(domains_list[order]):
+            results_text += '''
+            Results domain {}:
+            - Average R2 is {} ± {}
+            - {}% of full model R²
+            - Highest R2 is {}, in ROI_{} {} at x={}, y={}, z={}; LPI
+            '''.format(dom, r2_avg_doms[order][d], r2_std_doms[order][d], percent_fm[order][d],
+                        r2_max_doms[order][d], r2_max_doms_roi[order][d], r2_max_doms_roilab[order][d], r2_max_doms_coords[order][d,0], r2_max_doms_coords[order][d,1], r2_max_doms_coords[order][d,2])
+            
+        results_text += '''
+        Explained variance shared across multiple domains: {}%
+        '''.format(percent_shared)
+
+        # Save results
+        if save:
+            
+            path = '{}/cca_results/{}/group/single_doms/'.format(global_path, condition)
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            with open('{}cca_results/{}/group/results_{}.txt'.format(global_path, condition, condition), 'w') as res_file:
+                res_file.write(results_text)
+                
+            np.savetxt('{}cca_results/{}/group/fullmodel/significantROIs_{}.txt'.format(global_path, condition, condition), ROIs_sign)
+            np.savez('{}CCA_R2_group_singledoms'.format(path), results_group_sd=res_dom_mean, rois_list=ROIs_sign)
+            np.savez('{}CCA_R2_allsubs_singledoms'.format(path), results_subs_sd=res_dom, rois_list=ROIs_sign)
+            
+        # Save nifti
+        if save_nifti_opt:
+            for dom in range(res_dom.shape[-1]):
+                save_nifti(res_dom_final[:,:,dom], res_dom_final.shape[0], atlas_file, '{}CCA_R2_{}_{}.nii'.format(path, domains_list[dom], atlas_file), ROIs_sign)
